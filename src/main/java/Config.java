32c60f4f8d1298b6bc1f2fe8bb25c99a148717f5
Janek <jan.drychta@gmail.com>
2019-11-06 16:11:44 +0100

    Added method for inserting new data to database

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,3 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Config {
-    public static final String url = "DESKTOP-O33LS3J\\SQLEXPRESS";
+
+    private Connection conn;
+    public  Connection getConnect(){
+        String url = "jdbc:sqlserver://DESKTOP-O33LS3J\\SQLEXPRESS;databaseName=master;integratedSecurity=true;";
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection conn = DriverManager.getConnection(url);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+
+    }
+
+    public Connection getConn() {
+        return (conn != null) ? conn : getConnect();
+    }
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,56 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://DESKTOP-O33LS3J\\SQLEXPRESS;databaseName=master;integratedSecurity=true;";
-        Connection conn = DriverManager.getConnection(url);
-        System.out.println(conn);
 
-        Statement statement = conn.createStatement();
-        String sqlQuery = "Select * from Employees";
-        ResultSet rs = statement.executeQuery(sqlQuery);
-        while (rs.next()) {
-           /* Przykład
-           System.out.println(rs.getDate(8));*/
-            System.out.println(rs.getDate("StartJobDate"));
-        }
+    private static Connection conn;
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config();
+        conn = config.getConn();
+
+//        Statement statement = conn.createStatement();
+//        String sqlQuery = "Select * from Employees";
+//        ResultSet rs = statement.executeQuery(sqlQuery);
+//        while (rs.next()) {
+//            System.out.println(rs.getDate("StartJobDate"));
+//        }
         //insert
-        String insert = "Insert INTO Employees (LastName, FirstName, Address, City, Salary, Age, StartJobDate, Benefit) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        PreparedStatement ps = conn.prepareStatement(insert);
-        ps.setString(1, "Jarek");
-        ps.setString(2, "Garek");
-        ps.setString(3, "Bursztynowa");
-        ps.setString(4, "Wilno");
-        ps.setInt(5, 4500);
-        ps.setInt(6, 42);
-        ps.setDate(7, Date.valueOf("2018-10-15"));
-        ps.setInt(8, 1);
-        int rowInsert = ps.executeUpdate();
-        if(rowInsert > 0) {
-            System.out.println("Success!");
-        }
+//        String insert = "Insert INTO Employees (LastName, FirstName, Address, City, Salary, Age, StartJobDate, Benefit) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+//        PreparedStatement ps = conn.prepareStatement(insert);
+//        ps.setString(1, "Jarek");
+//        ps.setString(2, "Garek");
+//        ps.setString(3, "Bursztynowa");
+//        ps.setString(4, "Wilno");
+//        ps.setInt(5, 4500);
+//        ps.setInt(6, 42);
+//        ps.setDate(7, Date.valueOf("2018-10-15"));
+//        ps.setInt(8, 1);
+////        int rowInsert = ps.executeUpdate();
+////        if(rowInsert > 0) {
+////            System.out.println("Success!");
+////        }
     }
+ public void nowyWpis(String LastName, String FirstName, String Address, String City, int salary, int age, String start, int benefit) throws SQLException {
+
+     String insert = "Insert INTO Employees (LastName, FirstName, Address, City, Salary, Age, StartJobDate, Benefit) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+     PreparedStatement ps = conn.prepareStatement(insert);
+
+     ps.setString(1, LastName);
+     ps.setString(2, FirstName);
+     ps.setString(3, Address);
+     ps.setString(4, City);
+     ps.setInt(5, salary);
+     ps.setInt(6, age);
+     ps.setDate(7, Date.valueOf(start));
+     ps.setInt(8, benefit);
+     ps.executeUpdate();
+ }
 
 
 }
